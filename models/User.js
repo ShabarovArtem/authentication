21ac7faf703f1e1d45505e7f06e5061563b10962
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:21:47 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,11 @@
 const pool = require('../db');
 
-// Функция для получения пользователя по имени
 const getUserByUsername = async (username) => {
     const query = 'SELECT * FROM users WHERE username = $1';
     const result = await pool.query(query, [username]);
     return result.rows[0];
 };
 
-// Функция для создания нового пользователя
 const createUser = async (username, password, roles) => {
     const query = `
         INSERT INTO users (username, password, roles)
@@ -19,36 +17,11 @@ const createUser = async (username, password, roles) => {
     return result.rows[0];
 };
 
-// Функция для получения всех пользователей
 const getAllUsers = async () => {
     const query = 'SELECT * FROM users';
     const result = await pool.query(query);
     return result.rows;
 };
-
-// // Функция для создания таблицы пользователей
-// const createUsersTable = async () => {
-//     const query = `
-//         CREATE TABLE IF NOT EXISTS users (
-//             id SERIAL PRIMARY KEY,
-//             username VARCHAR(50) UNIQUE NOT NULL,
-//             password VARCHAR(100) NOT NULL,
-//             roles TEXT[]
-//         );
-//     `;
-//     await pool.query(query);
-// };
-//
-// // Функция для создания таблицы ролей
-// const createRolesTable = async () => {
-//     const query = `
-//         CREATE TABLE IF NOT EXISTS roles (
-//             id SERIAL PRIMARY KEY,
-//             value VARCHAR(50) UNIQUE NOT NULL
-//         );
-//     `;
-//     await pool.query(query);
-// };
 
 module.exports = {
     getUserByUsername,
